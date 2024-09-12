79b2ed81fa87add9647d8b9d14672212f44fecd6
Prashrijan Shrestha <141409513+prashrijan@users.noreply.github.com>
2024-09-12 02:48:03 +0000

    page1 animation uncommented

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
-// function page1Aniamtion(){
-//     var tl = gsap.timeline()
+function page1Aniamtion(){
+   var tl = gsap.timeline()
     
-//     tl.from("nav h1, .nav-links a, .nav-links button", {
-//         y: -20,
-//         opacity: 0,
-//         delay: 1,
-//         duration: .8,
-//         stagger: .15
-//     })
+    tl.from("nav h1, .nav-links a, .nav-links button", {
+        y: -20,
+        opacity: 0,
+        delay: 1,
+        duration: .8,
+       stagger: .15
+   })
     
-//     tl.from(".center .center-part1 h1" , {
-//         x: -200,
-//         opacity: 0,
-//         duration: .4
-//     }, "-=0.4")
+     tl.from(".center .center-part1 h1" , {
+         x: -200,
+         opacity: 0,
+         duration: .4
+     }, "-=0.4")
     
-//     tl.from(".center .center-part1 p", {
-//         x: -100, 
-//         opacity: 0,
-//         duration: .4
-//     })
+     tl.from(".center .center-part1 p", {
+         x: -100, 
+         opacity: 0,
+         duration: .4
+     })
     
-//     tl.from(".center .center-part1 button", {
-//         opacity: 0,
-//         duration: .4
-//     })
+     tl.from(".center .center-part1 button", {
+         opacity: 0,
+         duration: .4
+     })
     
-//     tl.from(".center .center-part2 img", {
-//         opacity: 0,
-//         duration: .5
-//     }, "-=1")
+     tl.from(".center .center-part2 img", {
+         opacity: 0,
+         duration: .5
+    }, "-=1")
     
-//     tl.from(".section-bottom img", {
-//         opacity: 0,
-//         duration: .6,
-//         stagger: 0.1,
-//         y: 30
-//     })
-// }
+     tl.from(".section-bottom img", {
+         opacity: 0,
+         duration: .6,
+         stagger: 0.1,
+         y: 30
+     })
+ }
 
+ page1Aniamtion()
     
 let tl = gsap.timeline({
     scrollTrigger: {
